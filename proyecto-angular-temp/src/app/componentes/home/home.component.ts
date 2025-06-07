e7c13880a6697fb0669d756970b42b816991d4fe
yeyocenteno <yeyocenteno@hotmail.com>
2025-06-07 16:56:36 -0600

    se cambio los componenentes de ofertadep, ubicacion, galeria y header

--- proyecto-angular-temp/src/app/componentes/home/home.component.ts
+++ proyecto-angular-temp/src/app/componentes/home/home.component.ts
@@ -3,10 +3,11 @@ import { OfertadepComponent } from '../ofertadep/ofertadep.component';
 import { GaleriaComponent } from '../galeria/galeria.component';
 import { UbicacionComponent } from '../ubicacion/ubicacion.component';
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [GaleriaComponent,OfertadepComponent,UbicacionComponent],
+  imports: [GaleriaComponent,OfertadepComponent,UbicacionComponent, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
