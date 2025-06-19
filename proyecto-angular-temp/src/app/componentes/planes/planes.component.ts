3c74e5d099de7e5de8715f1fcfc0695c3de19228
yeyocenteno <yeyocenteno@hotmail.com>
2025-06-19 02:18:55 -0600

    Se hicieron cambios en los siguientes archivos: - proyecto-angular-temp/src/app/componentes/nosotros/nosotros.component.css - proyecto-angular-temp/src/app/componentes/nosotros/nosotros.component.html - proyecto-angular-temp/src/app/componentes/planes/planes.component.css - proyecto-angular-temp/src/app/componentes/planes/planes.component.html - proyecto-angular-temp/src/app/componentes/planes/planes.component.ts - proyecto-angular-temp/src/app/componentes/suscripcion/suscripcion.component.css

--- proyecto-angular-temp/src/app/componentes/planes/planes.component.ts
+++ proyecto-angular-temp/src/app/componentes/planes/planes.component.ts
@@ -7,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrl: './planes.component.css'
 })
 export class PlanesComponent {
+  planBasico = [
+  { icon: 'assets/check.png', text: 'Acceso libre en horario regular' },
+  { icon: 'assets/check.png', text: 'Rutinas guiadas para principiantes' },
+  { icon: 'assets/check.png', text: '1 asesoría mensual con entrenador' },
+  { icon: 'assets/check.png', text: 'Acceso a 2 cursos grupales' },
+  { icon: 'assets/check.png', text: 'Acceso a cualquier instalación de la sucursal' },
+  { icon: 'assets/check.png', text: 'Plan nutricional general' },
+  { icon: 'assets/cancel.png', text: 'Descuentos en alimentos y bebidas' },
+  { icon: 'assets/cancel.png', text: 'Acceso VIP: zona de descanso, sauna, masajes' },
+  { icon: 'assets/cancel.png', text: 'Invitaciones a eventos y torneos' },
+];
+
+planIntermedio = [
+  { icon: 'assets/check.png', text: 'Acceso libre en horario extendido' },
+  { icon: 'assets/check.png', text: 'Rutinas personalizadas mensuales' },
+  { icon: 'assets/check.png', text: '2 asesorías mensuales con entrenador' },
+  { icon: 'assets/check.png', text: 'Acceso a 3 cursos grupales' },
+  { icon: 'assets/check.png', text: 'Evaluación de progreso bimestral' },
+  { icon: 'assets/check.png', text: 'Plan nutricional personalizado' },
+  { icon: 'assets/check.png', text: 'Descuentos en alimentos y bebidas' },
+  { icon: 'assets/cancel.png', text: 'Acceso VIP: zona de descanso, sauna, masajes' },
+  { icon: 'assets/cancel.png', text: 'Invitaciones a eventos y torneos' },
+];
+
+planAvanzado = [
+  { icon: 'assets/check.png', text: 'Acceso ilimitado al gimnasio' },
+  { icon: 'assets/check.png', text: 'Rutinas avanzadas cada 15 días' },
+  { icon: 'assets/check.png', text: '4 sesiones mensuales con entrenador' },
+  { icon: 'assets/check.png', text: 'Acceso a 5 cursos grupales' },
+  { icon: 'assets/check.png', text: 'Evaluación física mensual' },
+  { icon: 'assets/check.png', text: 'Plan nutricional específico' },
+  { icon: 'assets/check.png', text: 'Descuentos en alimentos y bebidas' },
+  { icon: 'assets/check.png', text: 'Acceso VIP: sauna, masajes, descanso' },
+  { icon: 'assets/check.png', text: 'Invitaciones a eventos y torneos' },
+];
 
 }
